f51966ac1c74a73f6de3849422f92a8ceb839108
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-11-22 00:11:15 +0700

    cleaning up

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="hero bg-white min-h-screen" style="background-image: url('/background.jpg');">
     <div class="hero-content flex-col lg:flex-row">
-      <img src="/landing.svg" alt="OCR Translate" class="transition-transform 
-            duration-300 ease-in-out hover:scale-105 m-2" />
+      <img src="/landing.svg" alt="OCR Translate" class="hover:scale-105 m-2 transition-transform duration-300 ease-in-out" />
       <div class="p-12">
-        <h1 class="text-5xl font-bold text-red-500 transition-transform 
-            duration-300 ease-in-out hover:scale-105 m-2">OCR - Translator</h1>
+        <h1 class="text-5xl font-bold text-red-500 hover:scale-105 m-2 transition-transform duration-300 ease-in-out">OCR - Translator</h1>
         <p class="py-6">
-          Press the button below to get started with OCR Translate. Upload an image containing text and we'll convert it
-          to text for you.
+          Press the button below to get started with OCR Translate. Upload an image containing English text, and we'll
+          convert it to Vietnamese text and generate a PDF for you.
         </p>
         <div class="flex space-x-6">
-          <nuxt-link to="/preview" class="btn btn-primary"><i 
-            class="fa-solid fa-rocket"></i> Get Started</nuxt-link>
-          <nuxt-link to="https://github.com/darklord1611/OCR-translate" class="btn btn-secondary"> <i
-              class="fa-brands fa-github"></i> About Us</nuxt-link>
+          <nuxt-link to="/preview" class="btn btn-primary"><i class="fa-solid fa-rocket"></i> Get Started</nuxt-link>
+          <nuxt-link to="https://github.com/darklord1611/OCR-translate" class="btn btn-secondary"><i class="fa-brands fa-github"></i> About Us</nuxt-link>
         </div>
       </div>
     </div>
--- frontend/pages/preview.vue
+++ frontend/pages/preview.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="pt-24 min-h-screen" style="background-image: url('/background.jpg');">
-
-    <div class="controls flex justify-center gap-x-4 gap-y-4 h-24 flex-wrap">
+    <div class="flex justify-center gap-4 h-24 flex-wrap">
       <select class="select select-bordered w-24 text-primary">
         <option>A4</option>
         <option>Letter</option>
@@ -18,31 +17,24 @@
       </select>
       <button v-if="images.length === 0" class="btn btn-primary btn-disabled">Convert</button>
       <button v-else class="btn btn-primary" @click="convertImage">Convert</button>
-
     </div>
 
     <div class="flex justify-center flex-wrap gap-8 p-8">
-      <div v-for="(image, index) in images" :key="index" class="flex justify-center flex-col items-center">
-
+      <div v-for="(image, index) in images" :key="index" class="flex justify-center flex-col items-center group relative">
         <img :src="image.url" alt="Uploaded Image"
-          class="h-72 w-full image-preview shadow-lg rounded-md object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl m-2">
-        <div
-          class="buttons absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex space-x-2">
-          <button class="btn btn-secondary btn-xs" @click="zoomImage(image.url)">
-            <i class="fas fa-search-plus"></i> <!-- Zoom Icon -->
+          class="h-72 w-full shadow-lg rounded-md object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl m-2">
+        <div class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex space-x-2">
+          <button class="btn btn-secondary btn-sm" @click="zoomImage(image.url)">
+            <i class="fas fa-search-plus"></i>
           </button>
-          <button class="btn btn-secondary btn-xs" @click="removeImage(index)">
-            <i class="fas fa-trash-alt"></i> <!-- Delete Icon -->
+          <button class="btn btn-secondary btn-sm" @click="removeImage(index)">
+            <i class="fas fa-trash-alt"></i>
           </button>
         </div>
-
-        <p class="text-primary overflow-hidden whitespace-wrap text-ellipsis">{{ image.name }}</p>
+        <p class="text-primary overflow-hidden text-ellipsis">{{ image.name }}</p>
       </div>
 
-      <div
-        class="h-72 w-72 flex items-center justify-center cursor-pointer 
-        text-primary hover:text-secondary transition duration-200 border-2 border-dashed border-primary hover:border-secondary rounded-md"
-        @click="triggerFileInput">
+      <div class="h-72 w-72 flex items-center justify-center cursor-pointer text-primary hover:text-secondary transition duration-200 border-2 border-dashed border-primary hover:border-secondary rounded-md" @click="triggerFileInput">
         <div class="flex flex-col justify-center items-center">
           <i class="fa-solid fa-image text-4xl"></i>
           <span class="text-center text-xl font-bold mt-2"><i class="fa-solid fa-plus"></i> Add Image</span>
